053037839b98ab92e8c9ef7b0a2ec359fbf87e29
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2020-07-05 16:41:21 -0300

    removing dummy code add @babel/polyfill updated @babel/cli

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { scream } from './utilities'
-
-const name = 'My simple App'
-
-console.log(scream(name))
--- src/utilities.js
+++
@@ -1,3 +0,0 @@
-export const scream = (text) => {
-    return `${text.toUpperCase()}!`
-}
